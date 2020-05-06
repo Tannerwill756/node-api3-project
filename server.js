51e2b16e954874b5257040c6ee87cec0e3d382e7
Tanner Williams <tannerwill756@gmail.com>
2020-05-06 16:25:46 -0400

    all gets working

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
-const express = require('express');
+const express = require("express");
+const postRouter = require("./posts/postRouter.js");
+const userRouter = require("./users/userRouter.js");
 
 const server = express();
 
-server.get('/', (req, res) => {
+server.use(express.json());
+server.use(logger);
+
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+// server.use("/api/posts", postRouter);
+
+server.use("/api/users", userRouter);
+
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const today = new Date().toISOString();
+  console.log(`[${today}] ${req.method} a ${req.url}`);
+  next();
+}
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,32 +1,89 @@
-const express = require('express');
+const express = require("express");
+const userData = require("./userDb");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
+  // userData.insert(req.body)
+  //   .then(post => {
+  //     if (req.body){
+  //       res.status(200).json(post)
+  //     }else{
+  //       res.status(404).json({ errorMessage: 'Please include'})
+  //     }
+  //   }).catch({ errorMessage: 'There was a problem uploading your post'})
+});
+
+router.post("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.post('/:id/posts', (req, res) => {
+router.get("/", (req, res) => {
+  userData
+    .get()
+    .then((users) => {
+      res.status(200).json(users);
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved" });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  userData
+    .getById(req.params.id)
+    .then((user) => {
+      console.log(user);
+      if (user != undefined) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({
+          message: "couldnt find the post with that id",
+        });
+      }
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ error: "The user information could not be retrieved" });
+    });
+});
+
+router.get("/:id/posts", (req, res) => {
+  userData
+    .getById(req.params.id)
+    .then((user) => {
+      if (user != undefined) {
+        userData
+          .getUserPosts(req.params.id)
+          .then((userPosts) => {
+            res.status(200).json(userPosts);
+          })
+          .catch((err) => {
+            res.status(500).json({
+              error: "There was an error retrieving that users posts",
+            });
+          });
+      } else {
+        res.status(404).json({
+          message: "couldnt find a user with that ID",
+        });
+      }
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ error: "The user information could not be retrieved" });
+    });
+});
+
+router.delete("/:id", (req, res) => {
   // do your magic!
 });
 
-router.get('/', (req, res) => {
-  // do your magic!
-});
-
-router.get('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
-});
-
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // do your magic!
 });
 
